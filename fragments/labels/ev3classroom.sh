f4fdb2b28f46b7a55c4fc210fe5ae5f93d19cf46
Robert Schroeder <robjschroeder@gmail.com>
2022-02-17 18:45:43 -0700

    Create ev3classroom.sh
    EV3 Classroom Lego Mindstorms

---
+++ fragments/labels/ev3classroom.sh
@@ -0,0 +1,8 @@
+ev3classroom)
+# added by robjschroeder
+    name="EV3 Classroom"
+    type="dmg"
+    downloadURL="https://education.lego.com/_/downloads/EV3_1.50_Global.dmg"
+    appNewVersion=""
+    expectedTeamID="RS3Y27K2T9"
+    ;;
