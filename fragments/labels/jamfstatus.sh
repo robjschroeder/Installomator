28178e2161a9e4dd430bdd4e31753b78445a0757
Robert Schroeder <robjschroeder@gmail.com>
2022-02-17 18:58:00 -0700

    Create jamfstatus.sh
    Jamf Status menu bar app

---
+++ fragments/labels/jamfstatus.sh
@@ -0,0 +1,8 @@
+jamfstatus)
+# added by robjschroeder
+    name="jamfStatus"
+    type="zip"
+    downloadURL="https://github.com/jamfprofessionalservices/jamfStatus/releases/download/current/jamfStatus.zip"
+    appNewVersion=""
+    expectedTeamID="PS2F6S478M"
+    ;;
